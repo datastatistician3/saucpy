4caecfa0340bb95f07451a5ddca60744f03ae985
Som Bohora <som-bohora@ouhsc.edu>
2017-08-15 11:13:37 -0500

    change setup.py

--- setup.py
+++ setup.py
@@ -49,14 +49,20 @@ setup(
         ],
     test_suite="tests",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.6',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        'Topic :: Text Processing :: Linguistic',
+      ]
+#    classifiers=[
+#        'Development Status :: 4 - Beta',
+#        'Intended Audience :: Developers',
+#        'License :: OSI Approved :: Apache Software License',
+#        'Programming Language :: Python :: 2.6',
+#        'Programming Language :: Python :: 2.7',
+#        'Programming Language :: Python :: 3.3',
+#        'Programming Language :: Python :: 3.4',
+#        'Programming Language :: Python :: 3.5',
+#        'Topic :: Software Development :: Libraries :: Python Modules'
+#    ]
 )
